4d677142c6cc6b8b29ec64b426a60906fe92d85a
Kevin Lau <thekevlau@gmail.com>
2015-07-01 00:46:48 -0400

    integrated heap for more detailed analytics, and added some more quotes

--- javascripts/main.js
+++ javascripts/main.js
@@ -26,7 +26,7 @@ var witty_quotes = [
     "In the darkest times, hope is something you give yourself. That is the meaning of inner strength.",
 
     // Pian Dao
-    "Creativity, versatility, intelligence. These are the traits that define a great swordsman.",
+    "Creativity, versatility, intelligence. These are the traits that define a great (swords)man.",
 
     // Roku
     "It was bitter work, but the results were worth it.",
